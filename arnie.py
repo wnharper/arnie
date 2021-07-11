600d19542e00d94f7d5e34c161ada58e7270394f
Warren Harper <warren.harper@gmail.com>
2021-07-10 19:02:01 -0700

    Created training data

--- arnie.py
+++ arnie.py
@@ -46,10 +46,10 @@ class Arnie(object):
         #self.traffic_sign_processor = ObjectsOnRoadProcessor(self)
         # lane_follower = DeepLearningLaneFollower()
 
-        self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        self.fourcc = cv2.VideoWriter_fourcc(*'H264')
         datestr = datetime.datetime.now().strftime("%y%m%d_%H%M%S")
-        self.video_orig = self.create_video_recorder('/home/pi/DeepPiCar/driver/data/car_video%s.avi' % datestr)
-        self.video_lane = self.create_video_recorder('/home/pi/DeepPiCar/driver/data/car_video_lane%s.avi' % datestr)
+        self.video_orig = self.create_video_recorder('/home/pi/DeepPiCar/driver/data/car_video%s.mp4' % datestr)
+        self.video_lane = self.create_video_recorder('/home/pi/DeepPiCar/driver/data/car_video_lane%s.mp4' % datestr)
         #self.video_objs = self.create_video_recorder('../data/tmp/car_video_objs%s.avi' % datestr)
 
         logging.info('Created Arnie')
